486fc20d6047bae462655ce1bba0593fb543c3c2
Андрей Шебаршов <122040698+23Andrey23@users.noreply.github.com>
2023-03-29 19:03:20 +0300

    Add files via upload

---
+++ Telegram/config.py
@@ -0,0 +1,7 @@
+TOKEN = '6224473761:AAEp8VjxukBwg8sQPJmmAASKfh69vRBn49I'
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
---
+++ Telegram/extensions.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from config import keys
+
+class APIException(Exception):
+    pass
+
+class Convert:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException('Введите разные вылюты')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалость обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалость обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалость обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+        return total_base * amount
