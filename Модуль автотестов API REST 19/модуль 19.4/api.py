d87710db9551168ddfc78924178a2b2f31cfa48b
Андрей Шебаршов <122040698+23Andrey23@users.noreply.github.com>
2023-04-11 22:50:43 +0300

    Update Автотесты API REST 19 module/module 19.4/api.py

--- Модуль автотестов API REST 19/модуль 19.4/api.py
+++ Модуль автотестов API REST 19/модуль 19.4/api.py
@@ -7,8 +7,11 @@ class ApiPet:
 
     def __init__(self):
         self.base_url = "https://petfriends.skillfactory.ru"
-
+        
     def get_api_key(self, email: str, password: str) -> json:
+         """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате
+        JSON с уникальным ключем пользователя, найденного по указанным email и паролем"""
+            
         headers = {
             "email": email,
             "password": password
@@ -24,6 +27,11 @@ class ApiPet:
         return status, result
 
     def get_list_pets(self, auth_key: json, filter: str) -> json:
+        """Метод делает запрос к API сервера и возвращает статус запроса и результат в формате JSON
+        со списком наденных питомцев, совпадающих с фильтром. На данный момент фильтр может иметь
+        либо пустое значение - получить список всех питомцев, либо 'my_pets' - получить список
+        собственных питомцев"""
+        
         headers = {'auth_key': auth_key['key']}
         filter = {'filter': filter}
 
@@ -37,6 +45,9 @@ class ApiPet:
         return status, result
 
     def post_api_pets(self, auth_key: json, name: str, animal_type: str, age: str, pet_photo: str) -> json:
+        """Метод отправляет (постит) на сервер данные о добавляемом питомце и возвращает статус
+        запроса на сервер и результат в формате JSON с данными добавленного питомца"""
+        
         data = {
             'name': name,
             'animal_type': animal_type,
@@ -57,6 +68,9 @@ class ApiPet:
         return status, result
 
     def put_api_pets(self, auth_key: json, pet_id: str, name: str, animal_type: str, age: str) -> json:
+         """Метод отправляет запрос на сервер о обновлении данных питомуа по указанному ID и
+        возвращает статус запроса и result в формате JSON с обновлённыи данными питомца"""
+            
         data = {
             'name': name,
             'age': age,
@@ -74,6 +88,10 @@ class ApiPet:
         return status, result
 
     def delete_api_pets(self, auth_key: json, pet_id: str) -> json:
+         """Метод отправляет на сервер запрос на удаление питомца по указанному ID и возвращает
+        статус запроса и результат в формате JSON с текстом уведомления о успешном удалении.
+        На сегодняшний день тут есть баг - в result приходит пустая строка, но status при этом = 200"""
+            
         headers = {'auth_key': auth_key['key']}
 
         res = requests.delete(self.base_url + 'api/pets/' + pet_id, headers=headers)
